db93e20737bbb7c447d3ab276a56011348ae7a4b
lain456 <iwakura.lain.pa@gmail.com>
2025-11-29 19:21:47 +0100

    tunispace-integration

---
+++ controller/comp/COMS/Coms_Config.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+const COMS2User_PATH = '/../../../model/COMS/User.php';
+const COMS2Conv_PATH = '/../../../model/COMS/Conversation.php';
+const COMS2Message_PATH = '/../../../model/COMS/Message.php';
+
+
+
+// controller to view
+Const COMS2B_PATH = '/../../../view/B/comp/COMS/index.php';
+const COMS2F_PATH ='/../../../view/F/comp/COMS/index.php';
+
+const COMS2L_PATH = '/../../../view/L/L_index.php';
+
+
+
+
+require_once __DIR__ . COMS2User_PATH;
+require_once __DIR__ . COMS2Conv_PATH;
+require_once __DIR__ . COMS2Message_PATH;
---
+++ view/B/B_index.php
@@ -0,0 +1,6 @@
+<?php
+require __DIR__ . '/B.html';
+
+
+?>
+
