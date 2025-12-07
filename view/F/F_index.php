1ecd84ce85bbf602bf841e02e32557c16354ab3e
lain456 <iwakura.lain.pa@gmail.com>
2025-12-07 20:37:10 +0100

    current update : added typing indicator added conv search updated UI - next update should include : online indicator open conv indicator reply like/reactions message (prob gonna be aborted) AI model integration (prob gonna be aborted)

--- view/F/F_index.php
+++
@@ -1,8 +0,0 @@
-<?php
-//FrontOffice
-
-
-require __DIR__ . '/F.html';
-
-//require __DIR__ . '/comp/ws_server/index.html';
-?>
--- view/L/L_index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-
-require __DIR__ . '/L.html';
-// login/register page
